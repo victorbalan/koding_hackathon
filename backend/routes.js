1a9eebe403085db78a65fc42883994e8b0bfaa3d
Victor Balan <victor.balan@catalysts.cc>
2014-12-06 19:13:03 +0200

    remove car mock function

--- backend/routes.js
+++ backend/routes.js
@@ -3,22 +3,6 @@ var CarTestingService = require('./services/carTestingService')
 
 
 module.exports = function(app){
-
-	app.post('/carmock', function(req, res){
-		var contents = req.body.fileContents
-
-		contents = "module.exports = function(){ \n" +
-					"	this.x = 'MAMA E GRASA' \n" +
-					"	this.getX = function(){ \n" +
-					"		return this.x \n" +
-					"	} \n" +
-					"}" 
-		var Car = stringToModule(contents)
-		var c = new Car()
-		
-		res.send(200)
-	})
-
 	app.post('/car/check', function(req, res){
 		var Car = stringToModule(req.body.data)
 		
