99532c83b69fa0af32afb76a46946587f5ebb43c
Victor Balan <victor.balan@catalysts.cc>
2014-12-07 20:54:22 +0200

    stylize race and garrage

--- frontend/controllers/game.js
+++ frontend/controllers/game.js
@@ -2,15 +2,6 @@ var request = require('request');
 var Car = require('../models/Car')
 
 exports.game = function(req, res) {	
-	// var roads = [{fromX: 100, fromY: 100, toX: 100, toY: 150},
-	// 	{fromX: 100, fromY: 150, toX: 150, toY: 150},
-	// 	{fromX: 150, fromY: 150, toX: 150, toY: 200},
-	// 	{fromX: 150, fromY: 200, toX: 100, toY: 250},
-	// 	{fromX: 100, fromY: 250, toX: 100, toY: 300}];
-	
-	// var maxLength = 301; 	
-
-
 	Car.findOne({userId: req.session.passport.user}, function(err, car){
 	    request.post('http://localhost:8081/car/test', 
 	    	{form :
