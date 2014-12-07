64f3a2fd8cf4a66dd1b9d07db1e93065495fb13f
Victor Balan <victor.balan@catalysts.cc>
2014-12-07 22:21:29 +0200

    add car file validation

--- frontend/controllers/garage.js
+++ frontend/controllers/garage.js
@@ -10,47 +10,69 @@ var fs = require('fs')
 	Car.find({userId: req.session.passport.user}, function(err, cars){
 		res.render('garage', {
 			title: 'Garage',
-			cars: cars
+			cars: cars,
+			error: ""
 		});
 	});
 };
 
 exports.postNewCar = function(req, res, next) {
-	// req.assert('fileToUpload', 'No file selected').notEmpty();
-	fs.readFile(req.files.file.path, 'utf8', function (err,data) {
-		if (err) {
-			console.log(err);
-		}else{
-		  	request.post('http://localhost:8081/car/info', 
-				{form: {data: data} },
-				function(error, response, body) {
-			        if (!error && response.statusCode == 200) {
-			        	console.log(body)
-			        	var jsonCar = JSON.parse(body).car
-			            var car = new Car({
-							userId: req.session.passport.user,
-							carModel: jsonCar.carModel,
-							name: req.body.carName,
-							maxSpeed: jsonCar.maxSpeed,
-							maxAcceleration: jsonCar.maxAcceleration,
-							breaking: jsonCar.maxBrakeing,
-							handling: 30,
-							grip: 70,
-							isFav: false,
-							maxSpeedReached: 101
-						})
-						car.save(function(err){
-							if(err){
-								console.log(err)
-								res.redirect('/error')
-							}else{	
-								res.redirect('/garage');
-							}
-						});
-			        }
-		   		});
-		}
-	});
+	// req.assert(req.files.file, 'No file selected').notEmpty();
+	var fileValid = true
+	if(req.files.file==undefined || req.files.file.originalFilename.split('.').pop()!=="js"){
+		fileValid = false
+	}
+	if(fileValid){
+		fs.readFile(req.files.file.path, 'utf8', function (err,data) {
+			if (err) {
+				console.log(err);
+			}else{
+			  	request.post('http://localhost:8081/car/info', 
+					{form: {data: data} },
+					function(error, response, body) {
+				        if (!error && response.statusCode == 200) {
+				        	console.log(body)
+				        	var jsonCar = JSON.parse(body).car
+				            var car = new Car({
+								userId: req.session.passport.user,
+								carModel: jsonCar.carModel,
+								name: req.body.carName,
+								maxSpeed: jsonCar.maxSpeed,
+								maxAcceleration: jsonCar.maxAcceleration,
+								breaking: jsonCar.maxBrakeing,
+								handling: 30,
+								grip: 70,
+								isFav: false,
+								maxSpeedReached: 101
+							})
+							car.save(function(err){
+								if(err){
+									console.log(err)
+									res.redirect('/error')
+								}else{	
+									res.redirect('/garage');
+								}
+							});
+				        }else{
+							Car.find({userId: req.session.passport.user}, function(err, cars){
+								res.render('garage', {
+									title: 'Garage',
+									cars: cars,
+									error: "Your uploaded file is corrupt. Please try again with another file."
+								});
+							});
+				        }
+			   		});
+			}
+		});
+	}else{
+		Car.find({userId: req.session.passport.user}, function(err, cars){
+			res.render('garage', {
+				title: 'Garage',
+				cars: cars,
+				error: "File format must be javascript."
+			});
+		});	}
 };
 
 exports.deleteCar = function(req, res){
