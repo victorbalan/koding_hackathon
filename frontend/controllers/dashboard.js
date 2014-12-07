bf334c7b388059a3063d2406de1fdcdfb4be1eca
Cristi Ilca <i_cristi01@yahoo.com>
2014-12-07 19:08:56 +0200

    cars owned, races played from db

--- frontend/controllers/dashboard.js
+++ frontend/controllers/dashboard.js
@@ -3,9 +3,17 @@
  * Dashboard form page.
  */
 var request = require('request');
+var Car = require('../models/Car');
+var Race = require('../models/Race');
 
 exports.getDashboard = function(req, res) {
-  res.render('dashboard', {
-    title: 'Dashboard'
-  });
+  Car.find({userId: req.session.passport.user}, function(err, cars){
+	Race.find({userId: req.session.passport.user}, function(errRace, race){
+			res.render('dashboard', {
+				title: 'Dashboard',
+				cars: cars,
+				race: race
+			});
+		});
+	});
 };
---
+++ frontend/models/Race.js
@@ -0,0 +1,11 @@
+//userid, carid, raceType, maxSpeed
+var mongoose = require('mongoose');
+
+var raceSchema = new mongoose.Schema({
+	userid: {type: String, required: true},
+	carid: {type: String, required: true},
+	raceType: {type: String, required: true},
+	maxSpeed: {type: Number, required: true}
+});
+
+module.exports = mongoose.model('Race', raceSchema);
